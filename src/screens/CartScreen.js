32baeb16fd8675fe324227241ac9827e14c6488c
AnkithaShettigar <107766536+AnkithaShettigar@users.noreply.github.com>
2023-01-03 17:27:57 +0530

    updated

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -19,7 +19,7 @@ export default function CartScreen() {
   } = state;
 
   const updateCartHandler = async (item, quantity) => {
-    const { data } = await axios.get(`/api/products/${item._id}`);
+    const { data } = await axios.get(`https://busy-cyan-salmon-coat.cyclic.app/api/products/${item._id}`);
     if (data.countInStack < quantity) {
       window.alert('Sorry,Product is Out of stock');
       return;
